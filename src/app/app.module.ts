37cd14a5833adaad75c08ab2bbdf7c4e64764b0c
Anveshika <anveshika.srivastava@epsilonium.com>
2024-06-29 10:50:54 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './master/layout/layout.component';
+import { BatchComponent } from './master/batch/batch.component';
+import { StudentComponent } from './master/student/student.component';
+import { ListComponent } from './master/batch/list/list.component';
+import { AddEditComponent } from './master/batch/add-edit/add-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    BatchComponent,
+    StudentComponent,
+    ListComponent,
+    AddEditComponent
   ],
   imports: [
     BrowserModule,
